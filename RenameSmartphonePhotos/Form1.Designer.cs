df06f9fbca01a7ad7d377f63777c51f95ca447d9
NataljaNeumann <56504749+NataljaNeumann@users.noreply.github.com>
2024-11-28 22:54:48 +0100

    Add files via upload

---
+++ RenameSmartphonePhotos/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿/*  RenameSmartphonePhotos tries to provide means in ordering your family photos based
+    on the date they have been taken
+
+    Copyright (C) 2024  NataljaNeumann@gmx.de
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+namespace RenameSmartphonePhotos
+{
+    partial class RenameFilesForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFolder = new System.Windows.Forms.TextBox();
+            this.buttonChooseFolder = new System.Windows.Forms.Button();
+            this.labelFolder = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // textBoxFolder
+            // 
+            this.textBoxFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFolder.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBoxFolder.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.textBoxFolder.Location = new System.Drawing.Point(12, 27);
+            this.textBoxFolder.Name = "textBoxFolder";
+            this.textBoxFolder.Size = new System.Drawing.Size(441, 20);
+            this.textBoxFolder.TabIndex = 0;
+            this.textBoxFolder.WordWrap = false;
+            // 
+            // buttonChooseFolder
+            // 
+            this.buttonChooseFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonChooseFolder.Location = new System.Drawing.Point(460, 25);
+            this.buttonChooseFolder.Name = "buttonChooseFolder";
+            this.buttonChooseFolder.Size = new System.Drawing.Size(42, 23);
+            this.buttonChooseFolder.TabIndex = 1;
+            this.buttonChooseFolder.Text = "...";
+            this.buttonChooseFolder.UseVisualStyleBackColor = true;
+            this.buttonChooseFolder.Click += new System.EventHandler(this.buttonChooseFolder_Click);
+            // 
+            // labelFolder
+            // 
+            this.labelFolder.AutoSize = true;
+            this.labelFolder.Location = new System.Drawing.Point(12, 8);
+            this.labelFolder.Name = "labelFolder";
+            this.labelFolder.Size = new System.Drawing.Size(36, 13);
+            this.labelFolder.TabIndex = 2;
+            this.labelFolder.Text = "Folder";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Rename";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RenameFilesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 113);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelFolder);
+            this.Controls.Add(this.buttonChooseFolder);
+            this.Controls.Add(this.textBoxFolder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "RenameFilesForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Rename Smartphone Photos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFolder;
+        private System.Windows.Forms.Button buttonChooseFolder;
+        private System.Windows.Forms.Label labelFolder;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ RenameSmartphonePhotos/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RenameSmartphonePhotos
+{
+    public partial class RenameFilesForm : Form
+    {
+        public RenameFilesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonChooseFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFolder.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Text.RegularExpressions.Regex regex = new System.Text.RegularExpressions.Regex(@"(.*)(20\d\d)(\d\d)(\d\d)[\s_-](.*)");
+            System.Text.RegularExpressions.Regex regex2 = new System.Text.RegularExpressions.Regex(@"\d\d\d\d-\d\d-\d\d\D(.*)");
+            System.Text.RegularExpressions.Regex regex3 = new System.Text.RegularExpressions.Regex(@"^(20\d\d)(\d\d)(\d\d)(\d\d\d\d\d\d)(\s?\(\d+\))?[\.](.*)");
+            System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(textBoxFolder.Text);
+            foreach (System.IO.FileInfo fi in di.GetFiles())
+            {
+                if (fi.Name.ToLower().Contains(".modd") || fi.Name.ToUpper().Contains("THUMBS.DB"))
+                    continue;
+                if (regex.IsMatch(fi.Name))
+                {
+                    //string strNewName = System.IO.Path.Combine(di.FullName, regex.Replace(fi.Name, "$2-$3-$4 $1$5"));
+                    string strNewName = System.IO.Path.Combine(di.FullName, regex.Replace(fi.Name, "$2-$3-$4 $1$5"));
+
+                    System.IO.FileInfo fi2 = new System.IO.FileInfo(fi.FullName + ".modd");
+                    string newModd = strNewName + ".modd";
+                    System.IO.FileInfo fi3 = new System.IO.FileInfo(System.IO.Path.Combine(System.IO.Path.Combine(fi.DirectoryName, "RestoreInfo"), fi.Name + ".chk"));
+                    if (!fi2.Exists && fi2.Name.ToLower().Contains(".m2ts"))
+                    {
+                        fi2 = new System.IO.FileInfo(fi.FullName.Replace(".m2ts", ".modd"));
+                        newModd = strNewName.Replace(".m2ts", ".modd");
+                    }
+                    if (!fi2.Exists && fi2.Name.ToLower().Contains(".mpg"))
+                    {
+                        fi2 = new System.IO.FileInfo(fi.FullName.Replace(".mpg", ".modd"));
+                        newModd = strNewName.Replace(".mpg", ".modd");
+                    }
+
+                    fi.MoveTo(strNewName);
+
+                    if (fi2.Exists)
+                        fi2.MoveTo(newModd);
+
+                    if (fi3.Exists)
+                    {
+                        string newName3 = System.IO.Path.Combine(System.IO.Path.Combine(di.FullName, "RestoreInfo"), strNewName.Substring(strNewName.LastIndexOf('\\') + 1) + ".chk");
+                        fi3.MoveTo(newName3);
+                    }
+                }
+                else
+                    if (regex3.IsMatch(fi.Name))
+                    {
+                        string strNewName = System.IO.Path.Combine(di.FullName, regex3.Replace(fi.Name, "$1-$2-$3 $4$5.$6"));
+                        //string strNewName = System.IO.Path.Combine(di.FullName, regex3.Replace(fi.Name, "$2-$3-$4 $5.$6"));
+
+                        System.IO.FileInfo fi2 = new System.IO.FileInfo(fi.FullName + ".modd");
+                        string newModd = strNewName + ".modd";
+                        System.IO.FileInfo fi3 = new System.IO.FileInfo(System.IO.Path.Combine(System.IO.Path.Combine(fi.DirectoryName, "RestoreInfo"), fi.Name + ".chk"));
+                        if (!fi2.Exists && fi2.Name.ToLower().Contains(".m2ts"))
+                        {
+                            fi2 = new System.IO.FileInfo(fi.FullName.Replace(".m2ts", ".modd"));
+                            newModd = strNewName.Replace(".m2ts", ".modd");
+                        }
+                        if (!fi2.Exists && fi2.Name.ToLower().Contains(".mpg"))
+                        {
+                            fi2 = new System.IO.FileInfo(fi.FullName.Replace(".mpg", ".modd"));
+                            newModd = strNewName.Replace(".mpg", ".modd");
+                        }
+
+                        fi.MoveTo(strNewName);
+
+                        if (fi2.Exists)
+                            fi2.MoveTo(newModd);
+
+                        if (fi3.Exists)
+                        {
+                            string newName3 = System.IO.Path.Combine(System.IO.Path.Combine(di.FullName, "RestoreInfo"), strNewName.Substring(strNewName.LastIndexOf('\\') + 1) + ".chk");
+                            fi3.MoveTo(newName3);
+                        }
+
+                    }
+                    else
+                        if (!regex2.IsMatch(fi.Name))
+                        {
+                            System.Text.RegularExpressions.Regex r = new System.Text.RegularExpressions.Regex(":");
+                            string strNewName = null;
+                            try {
+                                using (System.IO.FileStream fs = new System.IO.FileStream(fi.FullName, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+                                    using (Image myImage = Image.FromStream(fs, false, false))
+                                    {
+                                        try
+                                        {
+                                            System.Drawing.Imaging.PropertyItem propItem = myImage.GetPropertyItem(36867);
+                                            if (propItem != null && propItem.Value != null)
+                                            {
+                                                string dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                                                DateTime t;
+                                                if (DateTime.TryParse(dateTaken, out t))
+                                                {
+                                                    strNewName = System.IO.Path.Combine(di.FullName,
+                                                        string.Format("{0:D4}-{1:D2}-{2:D2} {3}", t.Year, t.Month, t.Day, fi.Name.Replace("DSC_", "").Replace("DSC ", "").Trim()));
+                                                }
+                                                else
+                                                {
+                                                    strNewName = System.IO.Path.Combine(di.FullName,
+                                                        string.Format("{0:D4}-{1:D2}-{2:D2} {3}", fi.LastWriteTime.Year, fi.LastWriteTime.Month, fi.LastWriteTime.Day, fi.Name));
+                                                }
+                                            }
+                                            else
+                                            {
+                                                strNewName = System.IO.Path.Combine(di.FullName,
+                                                    string.Format("{0:D4}-{1:D2}-{2:D2} {3}", fi.LastWriteTime.Year, fi.LastWriteTime.Month, fi.LastWriteTime.Day, fi.Name));
+                                            }
+                                        }
+                                        catch (ArgumentException)
+                                        {
+                                            strNewName = System.IO.Path.Combine(di.FullName,
+                                                string.Format("{0:D4}-{1:D2}-{2:D2} {3}", fi.LastWriteTime.Year, fi.LastWriteTime.Month, fi.LastWriteTime.Day, fi.Name));
+                                        }
+                                    }
+                            }
+                            catch (ArgumentException)
+                            {
+                                strNewName = System.IO.Path.Combine(di.FullName,
+                                    string.Format("{0:D4}-{1:D2}-{2:D2} {3}", fi.LastWriteTime.Year, fi.LastWriteTime.Month, fi.LastWriteTime.Day, fi.Name));
+                            }
+
+
+                            if (strNewName!=null)
+                            {
+                                System.IO.FileInfo fi2 = new System.IO.FileInfo(fi.FullName + ".modd");
+                                string newModd = strNewName + ".modd";
+                                System.IO.FileInfo fi3 = new System.IO.FileInfo(System.IO.Path.Combine(System.IO.Path.Combine(fi.DirectoryName, "RestoreInfo"), fi.Name + ".chk"));
+                                if (!fi2.Exists && fi2.Name.ToLower().Contains(".m2ts"))
+                                {
+                                    fi2 = new System.IO.FileInfo(fi.FullName.Replace(".m2ts", ".modd"));
+                                    newModd = strNewName.Replace(".m2ts", ".modd");
+                                }
+                                if (!fi2.Exists && fi2.Name.ToLower().Contains(".mpg"))
+                                {
+                                    fi2 = new System.IO.FileInfo(fi.FullName.Replace(".mpg", ".modd"));
+                                    newModd = strNewName.Replace(".mpg", ".modd");
+                                }
+
+                                fi.MoveTo(strNewName);
+
+                                if (fi2.Exists)
+                                    fi2.MoveTo(newModd);
+
+                                if (fi3.Exists)
+                                {
+                                    string newName3 = System.IO.Path.Combine(System.IO.Path.Combine(di.FullName, "RestoreInfo"), strNewName.Substring(strNewName.LastIndexOf('\\') + 1)+".chk");
+                                    fi3.MoveTo(newName3);
+                                }
+                            }
+                        }
+            }
+        }
+    }
+}
